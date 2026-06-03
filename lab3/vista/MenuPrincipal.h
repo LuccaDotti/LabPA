2be7e96fb67a0d32ff0af8fc4d99aae1df592214
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 22:04:54 -0300

    Update MenuPrincipal.h

--- lab3/vista/MenuPrincipal.h
+++ lab3/vista/MenuPrincipal.h
@@ -1,1 +1,24 @@
+#pragma once
 
+class AdminController;
+class EmpleadoController;
+class VentaController;
+
+class MenuPrincipal {
+
+private:
+
+    AdminController& adminController;
+    EmpleadoController& empleadoController;
+    VentaController& ventaController;
+
+public:
+
+    MenuPrincipal(
+        AdminController& adminController,
+        EmpleadoController& empleadoController,
+        VentaController& ventaController
+    );
+
+    void mostrar();
+};
