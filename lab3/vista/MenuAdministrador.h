dbc1ce2afa9e51e819ddd05f791d159748230248
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 22:03:25 -0300

    Update MenuAdministrador.h

--- lab3/vista/MenuAdministrador.h
+++ lab3/vista/MenuAdministrador.h
@@ -1,1 +1,18 @@
+#pragma once
 
+class AdminController;
+
+class MenuAdministrador {
+
+private:
+
+    AdminController& adminController;
+
+public:
+
+    MenuAdministrador(
+        AdminController& adminController
+    );
+
+    void mostrar();
+};
