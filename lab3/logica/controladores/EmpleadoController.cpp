96122aee528fc3587699b50d64243491aa8d3107
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 21:41:22 -0300

    Update EmpleadoController.cpp

--- lab3/logica/controladores/EmpleadoController.cpp
+++ lab3/logica/controladores/EmpleadoController.cpp
@@ -1,1 +1,66 @@
+#include "logica/controladores/EmpleadoController.h"
 
+EmpleadoController::EmpleadoController(
+    AdminController& adminController
+)
+: adminController(adminController)
+{
+}
+
+EmpleadoController::~EmpleadoController() {
+
+    for (Cliente* c : clientes)
+        delete c;
+
+    for (OrdenCompra* o : ordenesCompra)
+        delete o;
+}
+
+// ===== CLIENTES =====
+
+bool EmpleadoController::agregarCliente(
+    Cliente* cliente
+) {
+
+    clientes.push_back(cliente);
+
+    return true;
+}
+
+Cliente* EmpleadoController::buscarCliente(
+    int rut
+) const {
+
+    for (Cliente* c : clientes) {
+
+        ClienteRegistrado* cr =
+            dynamic_cast<ClienteRegistrado*>(c);
+
+        if (cr != nullptr &&
+            cr->getRut() == rut)
+            return c;
+    }
+
+    return nullptr;
+}
+
+vector<Cliente*> EmpleadoController::listarClientes() const {
+
+    return clientes;
+}
+
+// ===== ORDENES =====
+
+bool EmpleadoController::crearOrdenCompra(
+    OrdenCompra* orden
+) {
+
+    ordenesCompra.push_back(orden);
+
+    return true;
+}
+
+vector<OrdenCompra*> EmpleadoController::listarOrdenes() const {
+
+    return ordenesCompra;
+}
