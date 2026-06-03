7f856136de9358968bec358dd27efde14737484d
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 22:04:01 -0300

    Update MenuEmpleado.h

--- lab3/vista/MenuEmpleado.h
+++ lab3/vista/MenuEmpleado.h
@@ -1,1 +1,21 @@
+#pragma once
 
+class EmpleadoController;
+class AdminController;
+
+class MenuEmpleado {
+
+private:
+
+    EmpleadoController& empleadoController;
+    AdminController& adminController;
+
+public:
+
+    MenuEmpleado(
+        EmpleadoController& empleadoController,
+        AdminController& adminController
+    );
+
+    void mostrar();
+};
