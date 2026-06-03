6d81d0fa9b3dd8f2e18173144b8206225b288e97
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 21:41:35 -0300

    Update VentaController.cpp

--- lab3/logica/controladores/VentaController.cpp
+++ lab3/logica/controladores/VentaController.cpp
@@ -1,1 +1,49 @@
+#include "logica/controladores/VentaController.h"
 
+VentaController::VentaController(
+    AdminController& adminController
+)
+: adminController(adminController)
+{
+}
+
+VentaController::~VentaController() {
+
+    for (Venta* v : ventas)
+        delete v;
+
+    for (Calificacion* c : calificaciones)
+        delete c;
+}
+
+// ===== VENTAS =====
+
+bool VentaController::registrarVenta(
+    Venta* venta
+) {
+
+    ventas.push_back(venta);
+
+    return true;
+}
+
+vector<Venta*> VentaController::listarVentas() const {
+
+    return ventas;
+}
+
+// ===== CALIFICACIONES =====
+
+bool VentaController::agregarCalificacion(
+    Calificacion* calificacion
+) {
+
+    calificaciones.push_back(calificacion);
+
+    return true;
+}
+
+vector<Calificacion*> VentaController::listarCalificaciones() const {
+
+    return calificaciones;
+}
