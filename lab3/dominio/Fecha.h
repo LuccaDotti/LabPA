50e81d21cc28c8eb19cfdf08363a960debe9694f
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 21:23:16 -0300

    Create Fecha.h

---
+++ lab3/dominio/Fecha.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class Fecha {
+
+private:
+
+    int dia;
+    int mes;
+    int anio;
+
+    int hora;
+    int minutos;
+    int segundos;
+
+public:
+
+    Fecha(
+        int dia,
+        int mes,
+        int anio,
+        int hora,
+        int minutos,
+        int segundos
+    );
+
+    int getDia();
+    int getMes();
+    int getAnio();
+
+    int getHora();
+    int getMinutos();
+    int getSegundos();
+
+    void setDia(int dia);
+    void setMes(int mes);
+    void setAnio(int anio);
+
+    void setHora(int hora);
+    void setMinutos(int minutos);
+    void setSegundos(int segundos);
+};
