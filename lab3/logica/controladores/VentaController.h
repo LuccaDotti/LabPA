185bccf081182601018150507a51dfc13ce89d1b
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 21:40:38 -0300

    Update VentaController.h

--- lab3/logica/controladores/VentaController.h
+++ lab3/logica/controladores/VentaController.h
@@ -1,1 +1,40 @@
+#pragma once
 
+#include <vector>
+
+#include "logica/dominio/Venta.h"
+#include "logica/dominio/Calificacion.h"
+
+class AdminController;
+
+using namespace std;
+
+class VentaController {
+
+private:
+
+    vector<Venta*> ventas;
+    vector<Calificacion*> calificaciones;
+
+    AdminController& adminController;
+
+public:
+
+    VentaController(
+        AdminController& adminController
+    );
+
+    ~VentaController();
+
+    bool registrarVenta(
+        Venta* venta
+    );
+
+    vector<Venta*> listarVentas() const;
+
+    bool agregarCalificacion(
+        Calificacion* calificacion
+    );
+
+    vector<Calificacion*> listarCalificaciones() const;
+};
