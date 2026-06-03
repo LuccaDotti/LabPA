ab2b02ccdb60554ee2fc8bc94cf935a3e34dd16f
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-02 21:40:25 -0300

    Update EmpleadoController.h

--- lab3/logica/controladores/EmpleadoController.h
+++ lab3/logica/controladores/EmpleadoController.h
@@ -1,1 +1,45 @@
+#pragma once
 
+#include <vector>
+#include <string>
+
+#include "logica/dominio/Cliente.h"
+#include "logica/dominio/OrdenCompra.h"
+
+class AdminController;
+
+using namespace std;
+
+class EmpleadoController {
+
+private:
+
+    vector<Cliente*> clientes;
+    vector<OrdenCompra*> ordenesCompra;
+
+    AdminController& adminController;
+
+public:
+
+    EmpleadoController(
+        AdminController& adminController
+    );
+
+    ~EmpleadoController();
+
+    // Clientes
+
+    bool agregarCliente(Cliente* cliente);
+
+    Cliente* buscarCliente(int rut) const;
+
+    vector<Cliente*> listarClientes() const;
+
+    // Ordenes
+
+    bool crearOrdenCompra(
+        OrdenCompra* orden
+    );
+
+    vector<OrdenCompra*> listarOrdenes() const;
+};
