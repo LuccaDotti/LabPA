9d49aff33ec4247e31ff8d5efc0ea9ca991c1acb
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-08 19:34:18 -0300

    Update Calificacion.h

--- lab3/dominio/Calificacion.h
+++ lab3/dominio/Calificacion.h
@@ -1,1 +1,42 @@
+#pragma once
 
+#include <string>
+
+using namespace std;
+
+class Fecha;
+class Producto;
+
+class Calificacion
+{
+private:
+    int puntaje;
+    string comentarioOpcional;
+
+    Fecha* fecha;
+    Producto* producto;
+
+public:
+    Calificacion(
+        int puntaje,
+        const string& comentarioOpcional,
+        Fecha* fecha,
+        Producto* producto
+    );
+
+    ~Calificacion();
+
+    int getPuntaje() const;
+    void setPuntaje(int puntaje);
+
+    string getComentarioOpcional() const;
+    void setComentarioOpcional(
+        const string& comentarioOpcional
+    );
+
+    Fecha* getFecha() const;
+    void setFecha(Fecha* fecha);
+
+    Producto* getProducto() const;
+    void setProducto(Producto* producto);
+};
