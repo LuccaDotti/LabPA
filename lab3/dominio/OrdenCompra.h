e96f907c76a52864deca2be02b4428e47a82fda4
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-08 19:34:43 -0300

    Update OrdenCompra.h

--- lab3/dominio/OrdenCompra.h
+++ lab3/dominio/OrdenCompra.h
@@ -1,1 +1,57 @@
+#pragma once
 
+#include <vector>
+
+using namespace std;
+
+class Fecha;
+class Proveedor;
+class LineaCompra;
+
+enum EstadoOrden
+{
+    PENDIENTE,
+    RECIBIDA,
+    CANCELADA
+};
+
+class OrdenCompra
+{
+private:
+    Fecha* fechaEmision;
+    EstadoOrden estado;
+
+    Proveedor* proveedor;
+
+    vector<LineaCompra*> lineasCompra;
+
+public:
+    OrdenCompra(
+        Fecha* fechaEmision,
+        EstadoOrden estado,
+        Proveedor* proveedor
+    );
+
+    ~OrdenCompra();
+
+    Fecha* getFechaEmision() const;
+    void setFechaEmision(
+        Fecha* fechaEmision
+    );
+
+    EstadoOrden getEstado() const;
+    void setEstado(
+        EstadoOrden estado
+    );
+
+    Proveedor* getProveedor() const;
+    void setProveedor(
+        Proveedor* proveedor
+    );
+
+    vector<LineaCompra*> getLineasCompra() const;
+
+    void agregarLineaCompra(
+        LineaCompra* lineaCompra
+    );
+};
