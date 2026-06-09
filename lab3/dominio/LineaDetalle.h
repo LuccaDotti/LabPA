31c859756aab6e6f1f39bf20d96e27d562746ad9
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-09 20:44:47 -0300

    Update LineaDetalle.h

--- lab3/dominio/LineaDetalle.h
+++ lab3/dominio/LineaDetalle.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "Producto.h"
+using namespace std;
 
+class LineaDetalle {
+public:
+    LineaDetalle(Producto* producto, int cantidad, float precioUnitario);
+
+    Producto* getProducto() const;
+    int getCantidad() const;
+    float getPrecioUnitario() const;
+    float getSubtotal() const;
+
+private:
+    Producto* producto;
+    int cantidad;
+    float precioUnitario;
+};
