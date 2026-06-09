2a2111d10e0ddcf55da1095a9c620a59f4fe5c22
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-09 20:43:21 -0300

    Update ProveedorProducto.h

--- lab3/dominio/ProveedorProducto.h
+++ lab3/dominio/ProveedorProducto.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "Fecha.h"
+#include "Proveedor.h"
+#include "Producto.h"
+using namespace std;
 
+class ProveedorProducto {
+public:
+    ProveedorProducto(Proveedor* proveedor, Producto* producto, float precioCompra, const Fecha& fechaEntrega);
+
+    Proveedor* getProveedor() const;
+    Producto* getProducto() const;
+    float getPrecioCompra() const;
+    Fecha getFechaEntrega() const;
+
+    void setPrecioCompra(float precio);
+
+private:
+    Proveedor* proveedor;
+    Producto* producto;
+    float precioCompra;
+    Fecha fechaEntrega;
+};
