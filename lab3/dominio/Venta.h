04c9667f070099568f81e59ac8463406eab81384
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-09 20:41:53 -0300

    Update Venta.h

--- lab3/dominio/Venta.h
+++ lab3/dominio/Venta.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <vector>
+#include "Fecha.h"
+#include "Cliente.h"
+#include "LineaDetalle.h"
+using namespace std;
 
+class Venta {
+public:
+    Venta(const Fecha& fecha, Cliente* cliente);
+
+    Fecha getFecha() const;
+    Cliente* getCliente() const;
+
+    void agregarLineaDetalle(LineaDetalle* linea);
+    const vector<LineaDetalle*>& getLineas() const;
+
+    float getTotal() const;
+
+private:
+    Fecha fecha;
+    Cliente* cliente;
+    vector<LineaDetalle*> lineas;
+};
