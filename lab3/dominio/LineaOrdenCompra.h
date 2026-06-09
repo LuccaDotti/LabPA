e39856fd9741d26bf8bbe1e4e459aa08ab94d03f
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-09 20:44:08 -0300

    Update LineaOrdenCompra.h

--- lab3/dominio/LineaOrdenCompra.h
+++ lab3/dominio/LineaOrdenCompra.h
@@ -1,1 +1,29 @@
+#pragma once
+#include "Producto.h"
+#include "ProveedorProducto.h"
+using namespace std;
 
+class OrdenCompra;
+
+class LineaOrdenCompra {
+public:
+    LineaOrdenCompra(Producto* producto, ProveedorProducto* proveedorProducto,
+                     int cantidad, float precioUnitario);
+
+    OrdenCompra* getOrdenCompra() const;
+    Producto* getProducto() const;
+    ProveedorProducto* getProveedorProducto() const;
+    int getCantidad() const;
+    float getPrecioUnitario() const;
+    float getSubtotal() const;
+
+    void setCantidad(int cantidad);
+    void setOrdenCompra(OrdenCompra* orden);
+
+private:
+    OrdenCompra* ordenCompra;
+    Producto* producto;
+    ProveedorProducto* proveedorProducto;
+    int cantidad;
+    float precioUnitario;
+};
