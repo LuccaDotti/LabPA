3c70fc6b620eb939cfc933c0eed32e9c10ab008f
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-10 09:55:47 -0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,1 +1,24 @@
+#include "logica/controladores/AdminController.h"
+#include "logica/controladores/EmpleadoController.h"
+#include "logica/controladores/VentaController.h"
 
+#include "vista/MenuPrincipal.h"
+
+int main()
+{
+    AdminController adminCtrl;
+
+    EmpleadoController empleadoCtrl(adminCtrl);
+
+    VentaController ventaCtrl(adminCtrl);
+
+    MenuPrincipal menu(
+        adminCtrl,
+        empleadoCtrl,
+        ventaCtrl
+    );
+
+    menu.mostrar();
+
+    return 0;
+}
