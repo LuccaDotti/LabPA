50b9f49c0ce5bbd5572b1e0e4adc46de41580c8a
Nigel5002 <134554504+Nigel5002@users.noreply.github.com>
2026-06-09 21:22:06 -0300

    Delete lab3/include directory

--- lab3/include/Cliente.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-using namespace std;
-
-class Cliente {
-public:
-    virtual ~Cliente() = default;
-};
--- lab3/include/Estado.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace std;
-
-enum class Estado {
-    PENDIENTE,
-    APROBADA,
-    ENTREGADA,
-    CANCELADA
-};
