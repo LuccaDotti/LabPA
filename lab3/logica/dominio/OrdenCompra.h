3ed4c5ce3c274067bef1b41f2407b722fae8b80b
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-11 10:40:06 -0300

    Update OrdenCompra.h

--- lab3/logica/dominio/OrdenCompra.h
+++ lab3/logica/dominio/OrdenCompra.h
@@ -25,21 +25,42 @@ private:
 
     vector<LineaCompra*> lineasCompra;
 
+    // Lista de todas las órdenes de compra
+    vector<OrdenCompra*> ordenesCompra;
+
 public:
-    OrdenCompra(Fecha* fechaEmision, EstadoOrden estado, Proveedor* proveedor);
+    OrdenCompra(
+        Fecha* fechaEmision,
+        EstadoOrden estado,
+        Proveedor* proveedor
+    );
 
     ~OrdenCompra();
 
     Fecha* getFechaEmision() const;
-    void setFechaEmision(Fecha* fechaEmision);
+    void setFechaEmision(
+        Fecha* fechaEmision
+    );
 
     EstadoOrden getEstado() const;
-    void setEstado(EstadoOrden estado);
+    void setEstado(
+        EstadoOrden estado
+    );
 
     Proveedor* getProveedor() const;
-    void setProveedor(Proveedor* proveedor);
+    void setProveedor(
+        Proveedor* proveedor
+    );
 
     vector<LineaCompra*> getLineasCompra() const;
 
-    void agregarLineaCompra(LineaCompra* lineaCompra);
+    void agregarLineaCompra(
+        LineaCompra* lineaCompra
+    );
+
+    vector<OrdenCompra*> getOrdenesCompra() const;
+
+    void agregarOrdenCompra(
+        OrdenCompra* ordenCompra
+    );
 };
