667eb8f67c75c978b30967caf5ddadc6082b611d
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-11 10:52:52 -0300

    Update Venta.h

--- lab3/logica/dominio/Venta.h
+++ lab3/logica/dominio/Venta.h
@@ -3,16 +3,19 @@
 #include <vector>
 
 #include "logica/dominio/Fecha.h"
-#include "logica/dominio/ClienteRegistrado.h"
 #include "logica/dominio/LineaDetalle.h"
 
 using namespace std;
+
+class ClienteRegistrado;
 
 class Venta
 {
 private:
 
     Fecha fecha;
+
+    ClienteRegistrado* cliente;
 
     vector<LineaDetalle*> lineas;
 
@@ -22,12 +25,19 @@ private:
 public:
 
     Venta(
-        const Fecha& fecha
+        const Fecha& fecha,
+        ClienteRegistrado* cliente
     );
 
     ~Venta();
 
     Fecha getFecha() const;
+
+    ClienteRegistrado* getCliente() const;
+
+    void setCliente(
+        ClienteRegistrado* cliente
+    );
 
     void agregarLineaDetalle(
         LineaDetalle* linea
