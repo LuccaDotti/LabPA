61284837e41d82409bff6ba3f82d81120df90023
Nigel5002 <134554504+Nigel5002@users.noreply.github.com>
2026-06-18 17:47:12 -0300

    acomodando código

--- lab3/logica/controladores/EmpleadoController.cpp
+++ lab3/logica/controladores/EmpleadoController.cpp
@@ -1,12 +1,13 @@
 #include "logica/controladores/EmpleadoController.h"
 #include "logica/dominio/ClienteRegistrado.h"
+#include "logica/controladores/tipoRetorno.h"
 
 EmpleadoController::EmpleadoController(AdminController &adminController) : adminController(adminController) {}
 
 EmpleadoController::~EmpleadoController()
 {
 
-    for (Cliente *c : clientes)
+    for (ClienteRegistrado *c : clientes)
         delete c;
 
     for (OrdenCompra *o : ordenesCompra)
@@ -17,26 +18,75 @@ EmpleadoController::~EmpleadoController()
 // CLIENTES
 // ====================================
 
-bool EmpleadoController::agregarCliente(Cliente *cliente)
+TipoRet EmpleadoController::registrarCliente(int rut, string nombreCompleto, string correo, int nroPuerta, string calle, string ciudad, int contrasenia, float totalFacturado)
 {
+    if (buscarCliente(rut) != nullptr)
+    {
+        return TipoRet::ERROR_CLIENTE_EXISTENTE;
+    }
+
+    Direccion direccion(nroPuerta, calle, ciudad);
+    ClienteRegistrado *cliente = new ClienteRegistrado(rut, direccion, correo, nombreCompleto, contrasenia, totalFacturado);
     clientes.push_back(cliente);
 
-    return true;
+    return TipoRet::OK;
 }
 
-Cliente *EmpleadoController::buscarCliente(int rut) const
+TipoRet EmpleadoController::modificarCliente(int rut, string nombreCompleto, string correo, int nroPuerta, string calle, string ciudad, int contrasenia)
+{
+    if (buscarCliente(rut) == nullptr)
+    {
+        return TipoRet::ERROR_CLIENTE_INEXISTENTE;
+    }
+
+    for (int x = 0; x < clientes.size(); x++)
+    {
+        if (clientes[x]->getCorreo() == correo && clientes[x]->getRut() != rut)
+        {
+            return TipoRet::ERROR_CORREO_EXISTENTE;
+        }
+    }
+
+    ClienteRegistrado *cliente = buscarCliente(rut);
+    cliente->setNombreCompleto(nombreCompleto);
+    cliente->setCorreo(correo);
+    cliente->setDireccion(Direccion(nroPuerta, calle, ciudad));
+    cliente->setContrasenia(contrasenia);
+
+    return TipoRet::OK;
+}
+
+TipoRet EmpleadoController::registrarVenta(int rut, int codigoProducto, int cantidad)
+{
+    if (buscarCliente(rut) == nullptr)
+    {
+        return TipoRet::ERROR_CLIENTE_INEXISTENTE;
+    }
+
+    for (int x = 0; x < productos.size(); x++)
+    {
+        if (productos[x]->getCodigo() == codigoProducto)
+        {
+            return TipoRet::ERROR_PRODUCTO_INEXISTENTE;
+        }
+    }
+
+    return TipoRet::OK;
+}
+
+ClienteRegistrado *EmpleadoController::buscarCliente(int rut) const
 {
 
-for (ClienteRegistrado* c : clientes)
-{
-    if (c->getRut() == rut)
-        return c;
-}
+    for (ClienteRegistrado *c : clientes)
+    {
+        if (c->getRut() == rut)
+            return c;
+    }
 
     return nullptr;
 }
 
-vector<Cliente *> EmpleadoController::listarClientes() const
+vector<ClienteRegistrado *> EmpleadoController::listarClientes() const
 {
     return clientes;
 }
