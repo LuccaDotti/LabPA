98f8a8daed90b94dfabb0e5e8f10224e630b4c37
gabym3107 <gabriel.macedo@estudiantes.utec.edu.uy>
2026-06-19 14:10:51 -0300

    Update MenuAdministrador.cpp

--- lab3/vista/MenuAdministrador.cpp
+++ lab3/vista/MenuAdministrador.cpp
@@ -1,6 +1,6 @@
 #include "vista/MenuAdministrador.h"
 #include "logica/controladores/AdminController.h"
-#include "logica/tipos/TipoRet.h"
+#include "logica/controladores/tipoRetorno.h"
 
 #include <iostream>
 
@@ -117,10 +117,7 @@ void MenuAdministrador::agregarCategoria()
     getline(cin, descripcion);
 
     TipoRet ret =
-        adminController.agregarCategoria(
-            nombre,
-            descripcion
-        );
+        adminController.agregarCategoria(nombre, descripcion);
 
     if(ret == OK)
         cout << "Categoria agregada correctamente.\n";
@@ -145,11 +142,7 @@ void MenuAdministrador::modificarCategoria()
     getline(cin, nuevaDescripcion);
 
     TipoRet ret =
-        adminController.modificarCategoria(
-            nombreActual,
-            nuevoNombre,
-            nuevaDescripcion
-        );
+        adminController.modificarCategoria(nombreActual, nuevoNombre, nuevaDescripcion);
 
     if(ret == OK)
         cout << "Categoria modificada correctamente.\n";
@@ -197,17 +190,119 @@ void MenuAdministrador::menuProductos()
 
 void MenuAdministrador::agregarProducto()
 {
-    cout << "\nCU Agregar Producto\n";
+    int codigo;
+    string nombre;
+    string descripcion;
+    float precioUnitario;
+    int stockActual;
+    int stockMinimo;
+    string nombreCategoria;
+
+    cout << "\n===== AGREGAR PRODUCTO =====\n";
+
+    cout << "Codigo: ";
+    cin >> codigo;
+    cin.ignore();
+
+    cout << "Nombre: ";
+    getline(cin, nombre);
+
+    cout << "Descripcion: ";
+    getline(cin, descripcion);
+
+    cout << "Precio unitario: ";
+    cin >> precioUnitario;
+
+    cout << "Stock actual: ";
+    cin >> stockActual;
+
+    cout << "Stock minimo: ";
+    cin >> stockMinimo;
+    cin.ignore();
+
+    cout << "Categoria: ";
+    getline(cin, nombreCategoria);
+
+    Categoria* categoria =
+        adminController.buscarCategoria(nombreCategoria);
+
+    TipoRet ret =
+        adminController.agregarProducto(codigo, nombre, descripcion, precioUnitario, stockActual, stockMinimo, categoria);
+
+    if(ret == OK)
+        cout << "Producto agregado correctamente.\n";
+
+    else if(ret == ERROR_PRODUCTO_EXISTENTE)
+        cout << "Ya existe un producto con ese codigo.\n";
+
+    else if(ret == ERROR_STOCK_NEGATIVO)
+        cout << "El stock no puede ser negativo.\n";
 }
 
 void MenuAdministrador::modificarProducto()
 {
-    cout << "\nCU Modificar Producto\n";
+    int codigo;
+    string nombre;
+    string descripcion;
+    float precioUnitario;
+    int stockMinimo;
+    string nombreCategoria;
+
+    cout << "\n===== MODIFICAR PRODUCTO =====\n";
+
+    cout << "Codigo: ";
+    cin >> codigo;
+    cin.ignore();
+
+    cout << "Nuevo nombre: ";
+    getline(cin, nombre);
+
+    cout << "Nueva descripcion: ";
+    getline(cin, descripcion);
+
+    cout << "Nuevo precio: ";
+    cin >> precioUnitario;
+
+    cout << "Nuevo stock minimo: ";
+    cin >> stockMinimo;
+    cin.ignore();
+
+    cout << "Nueva categoria: ";
+    getline(cin, nombreCategoria);
+
+    Categoria* categoria =
+        adminController.buscarCategoria(nombreCategoria);
+
+    TipoRet ret =
+        adminController.modificarProducto(código, nombre, descripcion, precioUnitario, categoria, stockMinimo);
+
+    if(ret == OK)
+        cout << "Producto modificado correctamente.\n";
+
+    else if(ret == ERROR_PRODUCTO_INEXISTENTE)
+        cout << "Producto inexistente.\n";
+
+    else if(ret == ERROR_PRODUCTO_EXISTENTE)
+        cout << "Ya existe un producto con ese nombre.\n";
 }
 
 void MenuAdministrador::eliminarProducto()
 {
-    cout << "\nCU Eliminar Producto\n";
+    int codigo;
+
+    cout << "\n===== ELIMINAR PRODUCTO =====\n";
+
+    cout << "Codigo del producto: ";
+    cin >> codigo;
+
+    TipoRet ret =
+        adminController.eliminarProducto(codigo);
+
+    if(ret == OK)
+        cout << "Producto eliminado correctamente.\n";
+
+    else if(ret == ERROR_PRODUCTO_INEXISTENTE)
+        cout << "Producto inexistente.\n";
 }
 
 void MenuAdministrador::menuProveedores()
@@ -246,17 +341,116 @@ void MenuAdministrador::menuProveedores()
 
 void MenuAdministrador::altaProveedor()
 {
-    cout << "\nCU Alta Proveedor\n";
+    int rut;
+    string nombreEmpresa;
+    int telefono;
+    string nombreContacto;
+
+    cout << "\n===== ALTA DE PROVEEDOR =====\n";
+
+    cout << "RUT: ";
+    cin >> rut;
+    cin.ignore();
+
+    cout << "Nombre empresa: ";
+    getline(cin, nombreEmpresa);
+
+    cout << "Telefono: ";
+    cin >> telefono;
+    cin.ignore();
+
+    cout << "Nombre contacto: ";
+    getline(cin, nombreContacto);
+
+    TipoRet ret =
+        adminController.agregarProveedor(rut, nombreEmpresa, telefono, nombreContacto);
+
+    if(ret == OK)
+        cout << "Proveedor agregado correctamente.\n";
+
+    else if(ret == ERROR_PROVEEDOR_EXISTENTE)
+        cout << "Ya existe un proveedor con ese RUT.\n";
 }
 
 void MenuAdministrador::modificarProveedor()
 {
-    cout << "\nCU Modificar Proveedor\n";
+    int rut;
+    string nombreEmpresa;
+    int telefono;
+    string nombreContacto;
+
+    cout << "\n===== MODIFICAR PROVEEDOR =====\n";
+
+    cout << "RUT: ";
+    cin >> rut;
+    cin.ignore();
+
+    cout << "Nuevo nombre empresa: ";
+    getline(cin, nombreEmpresa);
+
+    cout << "Nuevo telefono: ";
+    cin >> telefono;
+    cin.ignore();
+
+    cout << "Nuevo nombre contacto: ";
+    getline(cin, nombreContacto);
+
+    TipoRet ret =
+        adminController.modificarProveedor(rut, nombreEmpresa, telefono, nombreContacto);
+
+    if(ret == OK)
+        cout << "Proveedor modificado correctamente.\n";
+
+    else if(ret == ERROR_PROVEEDOR_INEXISTENTE)
+        cout << "Proveedor inexistente.\n";
 }
 
 void MenuAdministrador::registrarProveedorProducto()
 {
-    cout << "\nCU Registrar Proveedor-Producto\n";
+    int rutProveedor;
+    int codigoProducto;
+    float precioCompra;
+
+    int dia;
+    int mes;
+    int anio;
+
+    cout << "\n===== ASOCIAR PROVEEDOR - PRODUCTO =====\n";
+
+    cout << "RUT proveedor: ";
+    cin >> rutProveedor;
+
+    cout << "Codigo producto: ";
+    cin >> codigoProducto;
+
+    cout << "Precio de compra: ";
+    cin >> precioCompra;
+
+    cout << "Dia entrega: ";
+    cin >> dia;
+
+    cout << "Mes entrega: ";
+    cin >> mes;
+
+    cout << "Anio entrega: ";
+    cin >> anio;
+
+    Fecha* fechaEntrega =
+        new Fecha(dia, mes, anio);
+
+    TipoRet ret =
+        adminController.asociarProveedorProducto(rutProveedor, codigoProducto, precioCompra, fechaEntrega);
+
+    delete fechaEntrega;
+
+    if(ret == OK)
+        cout << "Asociacion registrada correctamente.\n";
+
+    else if(ret == ERROR_PROVEEDOR_INEXISTENTE)
+        cout << "Proveedor inexistente.\n";
+
+    else if(ret == ERROR_PRODUCTO_INEXISTENTE)
+        cout << "Producto inexistente.\n";
 }
 
 void MenuAdministrador::menuCalificaciones()
