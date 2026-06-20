4377ca3d5955451a4d35e543bc6964ad0d8d29bb
Nigel5002 <134554504+Nigel5002@users.noreply.github.com>
2026-06-20 00:43:27 -0300

    Refactor Fecha class member variable declarations
    Removed redundant member variable declarations in the Fecha class.

--- lab3/logica/dominio/Fecha.h
+++ lab3/logica/dominio/Fecha.h
@@ -1,25 +1,17 @@
 #pragma once
 
-class Fecha {
-
+class Fecha
+{
 private:
-
-    int dia;
-    int mes;
-    int anio;
-
-    int hora;
-    int minutos;
-    int segundos;
+    int dia, mes, anio, hora, minutos, segundos;
 
 public:
-
+    Fecha();
     Fecha(int dia, int mes, int anio, int hora, int minutos, int segundos);
 
     int getDia();
     int getMes();
     int getAnio();
-
     int getHora();
     int getMinutos();
     int getSegundos();
@@ -27,7 +19,6 @@ public:
     void setDia(int dia);
     void setMes(int mes);
     void setAnio(int anio);
-
     void setHora(int hora);
     void setMinutos(int minutos);
     void setSegundos(int segundos);
